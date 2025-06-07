e1989b5e688cb1fab9c37a69f2296966d49b2740
Priyanshu Tandon <143897258+Priyanshut972@users.noreply.github.com>
2025-06-07 19:04:51 +0530

    Update application.py

--- application.py
+++ application.py
@@ -1,7 +1,7 @@
 from flaskr import create_app
 from flask_cors import CORS
 
-app = Flask(__name__)
-CORS(app, origins=["https://vercel-frontend-nine-dun.vercel.app/"])
 
+CORS(app, origins=["https://vercel-frontend-nine-dun.vercel.app/"])
+app = create_app()
 
